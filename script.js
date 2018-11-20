2466d7b7d9e7e108c1c3886cc3055c42f55cd141
svrakata <svrakata@gmail.com>
2018-11-20 14:17:31 +0200

    update

--- script.js
+++ script.js
@@ -3,15 +3,17 @@
 class ProgressBar {
     constructor(options) {
         this.defaults = {
-            barHeight: 50,
-            barColor: '#22ff88',
-            sliderColor: '#44aa44',
-            sliderWidth: 360,
             delayTime: 3000,
+            durationSlider: {
+                step: 30,
+                min: 30,
+                max: 600,
+                value: 30,
+            },
         };
-        
+
         this.length = options.length;
-        
+
         this.actionDelayTime = options.delayTime || this.defaults.delayTime;
         this.barHeight = options.barHeight || this.defaults.barHeight;
         this.barColor = options.barColor || this.defaults.barColor;
@@ -32,21 +34,30 @@ class ProgressBar {
             moveHandler: this.moveSlider.bind(this),
             releaseHandler: this.sliderReleased.bind(this),
             actionOnRelease: (time) => console.warn('Please set a callback as an action!', time),
-            width: options.sliderWidth || this.defaults.sliderWidth,
         };
 
         this.durationSlider = {
             el: document.createElement('input'),
+            attr: this.defaults.durationSlider,
         }
 
         this.mouse = {
             isDown: false,
             lastPosition: null,
-            sliderLeftPosition: null, 
+            sliderLeftPosition: null,
         };
 
-        this.setupElements(options);
-        this.addListeners();
+        this.timers = {
+            durationRecording: new Timer('Duration of the recording:', this.length),
+            durationSection: new Timer('Duration of the section:', this.durationSlider.attr.value),
+            startTimeSection: new Timer('Start time of the section:', 0),
+            endTimeSection: new Timer('End time of the section:', 0),
+        };
+
+        // the order of executing these functions is strict
+        // i don't like that 
+
+        this.setupElements();
         this.addToDom();
 
         this.bar.boundaries = this.getBoundaries(this.bar.el);
@@ -54,39 +65,49 @@ class ProgressBar {
         this.secondsPerPixels = this.getSecondsPerPixels();
         this.pixelsPerSecond = this.getPixelsPerSecond();
 
-        this.setSliderWidth(this.slider.width);
-        this.slider.boundaries = this.getBoundaries(this.slider.el);
+        this.setSliderWidth(this.durationSlider.el.value);
         this.setMarkers();
 
         // change the position of the markers and the px per sec and sec per px values on screen resize
         window.addEventListener('resize', _.throttle(this.resetBarOnWindowResize.bind(this)), 150, { 'leading': true });
-    }
 
-    setupElements(options) {
-        // bar setup
-        this.bar.el.classList.add('progress__bar');
-        this.bar.el.style.backgroundColor = options.barColor || this.defaults.barColor;
-        this.bar.el.style.height = `${options.barHight || this.defaults.barHeight}px`;
-
-        // slider setup
-        this.slider.el.classList.add('progress__slider');
-        this.slider.el.style.backgroundColor = options.sliderColor || this.defaults.sliderColor;
-
-        // duration input setup
-        this.durationSlider.el.classList.add('progress__duration');
-        this.durationSlider.el.setAttribute('type', 'range');
+        this.helpers = {
+            passStringPxReturnNumber: (pixelsString) => (+(pixelsString.split('px')[0])),
+        };
 
     }
 
-    addListeners() {
-        this.slider.el.addEventListener('mousedown', this.sliderPressedDown.bind(this));
-        this.bar.el.addEventListener('click', this.moveSliderOnClick.bind(this));
+    setupElements() {
+        this.barElSetup(this.bar);
+        this.sliderElSetup(this.slider);
+        this.durationElSetup(this.durationSlider);
+    }
+
+    barElSetup(node) {
+        node.el.classList.add('progress__bar');
+        node.el.addEventListener('click', this.moveSliderOnClick.bind(this));
+    }
+
+    sliderElSetup(node) {
+        node.el.classList.add('progress__slider');
+        node.el.addEventListener('mousedown', this.sliderPressedDown.bind(this));
+    }
+
+    durationElSetup(node) {
+        node.el.classList.add('progress__duration');
+        node.el.setAttribute('type', 'range');
+        node.el.value = node.attr.value;
+        node.el.min = node.attr.min;
+        node.el.max = node.attr.max;
+        node.el.step = node.attr.step;
+        node.el.addEventListener('change', this.changeSliderWidth.bind(this));
     }
 
     addToDom() {
         this.bar.el.appendChild(this.slider.el);
         this.container.appendChild(this.bar.el);
         this.container.appendChild(this.durationSlider.el);
+        Object.keys(this.timers).forEach(e => this.container.appendChild(this.timers[e].el.container));
     }
 
     getBoundaries(element) {
@@ -94,31 +115,42 @@ class ProgressBar {
     }
 
     setSliderWidth(seconds) {
-        const px = seconds * this.pixelsPerSecond;
-        this.slider.el.style.width = `${px}px`;
-        this.slider.boundaries = this.getBoundaries(this.slider.el);
+        const px = parseInt(seconds) * this.pixelsPerSecond;
+        const action = () => {
+            this.slider.el.style.width = `${px}px`;
+            // update the slider boundaries inside the animationFrame 'cause it's called asyncly
+            this.slider.boundaries = this.getBoundaries(this.slider.el);
+        };
+        requestAnimationFrame(action);
+    }
+
+    changeSliderWidth(e) {
+        const value = e.target.value;
+        this.timers.durationSection.updateTime(value);
+        this.setSliderWidth(value);
     }
 
     sliderPressedDown(e) {
         this.mouse.isDown = true;
         this.mouse.lastPosition = e.clientX;
-        this.mouse.sliderLeftPosition = giveStringPxReturnNumber(this.slider.el.style.left);
+        this.mouse.sliderLeftPosition = this.helpers.passStringPxReturnNumber(this.slider.el.style.left);
         window.addEventListener('mousemove', this.slider.moveHandler);
         window.addEventListener('mouseup', this.slider.releaseHandler);
     }
 
     moveSlider(e) {
-        let distance = e.clientX - this.mouse.lastPosition;
+        const distance = e.clientX - this.mouse.lastPosition;
+        const sliderNewLeft = this.mouse.sliderLeftPosition + distance;
 
-        if (this.mouse.sliderLeftPosition + distance < 0) {
+        if (sliderNewLeft < 0) {
             return this.setLeftPositionSlider(0);
         }
 
-        if (this.mouse.sliderLeftPosition + distance > this.bar.boundaries.width - this.slider.boundaries.width) {
+        if (sliderNewLeft > this.bar.boundaries.width - this.slider.boundaries.width) {
             return this.setLeftPositionSlider(this.bar.boundaries.width - this.slider.boundaries.width);
         }
 
-        this.setLeftPositionSlider(this.mouse.sliderLeftPosition + distance);
+        this.setLeftPositionSlider(sliderNewLeft);
     }
 
     moveSliderOnClick(e) {
@@ -144,16 +176,16 @@ class ProgressBar {
     }
 
     setLeftPositionSlider(position) {
-        this.slider.el.style.left = `${position}px`;
+        requestAnimationFrame(() => this.slider.el.style.left = `${position}px`);
     }
 
-    setSliderAtPositionInSnds(snds = 234) {
+    setSliderAtPositionInSnds(snds = 0) {
         const pxs = snds * this.pixelsPerSecond;
         const start = 0;
         const end = this.bar.boundaries.width - this.slider.boundaries.width;
 
         if (pxs >= end) {
-            this.setLeftPositionSlider(end);      
+            this.setLeftPositionSlider(end);
         } else if (pxs < start) {
             this.setLeftPositionSlider(0);
         } else {
@@ -175,23 +207,23 @@ class ProgressBar {
         marker.style.left = position * this.pixelsPerSecond + 'px';
 
         if (tall) {
-            marker.style.height = '30px';
+            marker.classList.add('progress__marker--tall');
         }
+
         this.bar.el.appendChild(marker);
     };
 
     setMarkers() {
-        const oneHourMarkers = this.length / (60 * 60);
         const tenMinutesMarkers = this.length / (60 * 10);
-
-        // set markers for each hour
-        for (let i = 0; i <= oneHourMarkers; i++) {
-            this.createAndAddMarker(i * 3600, true);
-        }
 
         // sets markers for each 10 minutes
         for (let i = 0; i <= tenMinutesMarkers; i++) {
-            this.createAndAddMarker(i * 600);
+            if (i % 6 === 0) {
+                this.createAndAddMarker(i * 600, true);
+            } else {
+                this.createAndAddMarker(i * 600);
+            }
+
         }
     }
 
@@ -211,7 +243,7 @@ class ProgressBar {
     }
 
     getTimeAccordingSliderPosition() {
-        return giveStringPxReturnNumber(this.slider.el.style.left) * this.secondsPerPixels;
+        return this.helpers.passStringPxReturnNumber(this.slider.el.style.left) * this.secondsPerPixels;
     }
 
     setActionOnSliderRelease(action) {
@@ -219,29 +251,66 @@ class ProgressBar {
     }
 }
 
+class Timer {
+    constructor(label = '', time = '') {
 
-const giveStringPxReturnNumber = (pixelsString) => {
-    return (+(pixelsString.split('px')[0]));
-};
+        this.helpers = {
+            passSecondsReturnTime: (seconds) => {
+                let hours = Math.floor(seconds / 3600);
+                let minutes = Math.floor((seconds % 3600) / 60);
+                let secondsLeft = seconds - (hours * 3600) - (minutes * 60);
 
-const getSecondsReturnTime = (seconds) => {
-    let hours = Math.floor(seconds / 3600);
-    let minutes = Math.floor((seconds % 3600) / 60);
-    let secondsLeft = seconds - (hours * 3600) - (minutes * 60);
+                if (hours < 9) {
+                    hours = `0${hours}`;
+                }
 
-    if (hours < 9) {
-        hours = `0${hours}`;
+                if (minutes < 9) {
+                    minutes = `0${minutes}`;
+                }
+
+                if (secondsLeft < 9) {
+                    secondsLeft = `0${secondsLeft}`;
+                }
+
+                return `${hours}h. ${minutes}m. ${secondsLeft}s.`;
+            },
+        }
+
+        this.label = label;
+        this.time = this.helpers.passSecondsReturnTime(time);
+
+        this.el = this.createEl();
+
+
     }
 
-    if (minutes < 9) {
-        minutes = `0${minutes}`;
+    createEl() {
+        const container = document.createElement('div');
+        const label = document.createElement('span');
+        const time = document.createElement('span');
+
+        container.classList.add('progress__timer');
+        label.classList.add('progress__label');
+        time.classList.add('progress__time');
+
+        label.innerHTML = this.label;
+        time.innerHTML = this.time;
+
+        container.appendChild(label);
+        container.appendChild(time);
+
+        return {
+            container,
+            label,
+            time,
+        };
     }
 
-    if (secondsLeft < 9) {
-        secondsLeft = `0${secondsLeft}`;
+    changeLabel(newLabel = '') {
+        this.el.label.innerHTML = newLabel;
     }
 
-    return `${hours}ч. ${minutes}м. ${secondsLeft}с.`;
-};
-
-
+    updateTime(newTime = 0) {
+        this.el.time.innerHTML = this.helpers.passSecondsReturnTime(newTime);
+    }
+}
